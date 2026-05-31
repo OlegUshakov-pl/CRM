f1199eb2c26d2807a33e3a9413dab1b6f8f11278
Oleg Ushakov <olegushakov38@gmail.com>
2026-05-31 18:11:36 +0200

    Update version.py

--- core/version.py
+++ core/version.py
@@ -5,11 +5,4 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 def read_version():
-    try:
-        count = subprocess.run(
-            ['git', 'rev-list', '--count', 'HEAD'],
-            capture_output=True, text=True, cwd=BASE_DIR
-        ).stdout.strip()
-        return f'1.1.{count}'
-    except Exception:
-        return '1.1.0'
+    return '1.2.001'
