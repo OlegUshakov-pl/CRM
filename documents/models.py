9685633f023cb3f370d771f6c847c3df9e493033
Oleg Ushakov <olegushakov38@gmail.com>
2026-06-05 05:45:45 +0200

    Update models.py
    Signed-off-by: Oleg Ushakov <olegushakov38@gmail.com>

--- documents/models.py
+++ documents/models.py
@@ -12,7 +12,16 @@ def document_upload_to(instance, filename):
     if instance.project:
         safe_number = sanitize_folder_name(instance.project.number) if instance.project.number else instance.project.slug
         safe_name = sanitize_folder_name(instance.project.name)
-        return os.path.join(f'{safe_number}_{safe_name}_Project', f'{safe_number}_documents', filename)
+        file_type = getattr(instance, 'file_type', 'other')
+        subfolder_map = {
+            'drawings': f'{safe_number}_drawings',
+            'models_3d': f'{safe_number}_models',
+            'documents': f'{safe_number}_documents',
+            'photos': f'{safe_number}_documents',
+            'other': f'{safe_number}_documents',
+        }
+        subfolder = subfolder_map.get(file_type, f'{safe_number}_documents')
+        return os.path.join(f'{safe_number}_{safe_name}_Project', subfolder, filename)
     return os.path.join('_no_project', filename)
 
 
@@ -57,7 +66,15 @@ class Document(models.Model):
         from projects.utils import get_project_subfolder_path
         if self.project:
             safe_number = sanitize_folder_name(self.project.number) if self.project.number else self.project.slug
-            folder = get_project_subfolder_path(self.project, f'{safe_number}_documents')
+            subfolder_map = {
+                'drawings': f'{safe_number}_drawings',
+                'models_3d': f'{safe_number}_models',
+                'documents': f'{safe_number}_documents',
+                'photos': f'{safe_number}_documents',
+                'other': f'{safe_number}_documents',
+            }
+            subfolder = subfolder_map.get(self.file_type, f'{safe_number}_documents')
+            folder = get_project_subfolder_path(self.project, subfolder)
             if folder and self.file:
                 return os.path.join(folder, self.filename)
         return os.path.join(settings.DOCUMENTS_ROOT, self.file.name) if self.file else ''
