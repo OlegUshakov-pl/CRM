6ccaeb7f012a5b925c04c69671f0f4e736aee1aa
Oleg Ushakov <olegushakov38@gmail.com>
2026-06-09 06:11:15 +0200

    Update forms.py

--- projects/forms.py
+++ projects/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from .models import Project
+from companies.models import Company
+
+
 class ProjectForm(forms.ModelForm):
     number = forms.CharField(
         required=True,
@@ -18,3 +21,7 @@ class ProjectForm(forms.ModelForm):
             'end_date': forms.DateInput(attrs={'type': 'date', 'class': 'w-full px-4 py-2.5 border border-[#e8e8e8] text-[14px] outline-none focus:border-violet-300 transition-colors'}),
             'budget': forms.NumberInput(attrs={'class': 'w-full px-4 py-2.5 border border-[#e8e8e8] text-[14px] outline-none focus:border-violet-300 transition-colors', 'placeholder': '0.00'}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['company'].queryset = Company.objects.filter(is_active=True)
